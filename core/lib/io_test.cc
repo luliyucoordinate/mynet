7ff6bae5237360b5077f9386566df9b1119821a2
luliyu <coordinate@live.com>
2021-10-30 17:04:39 +0800

    update io for ownership

--- core/lib/io_test.cc
+++ core/lib/io_test.cc
@@ -14,19 +14,21 @@ namespace mynet {
 class IOTest : public ::testing::Test {};
 
 TEST_F(IOTest, ReadWriteNetParamsFromTextFile) {
-  auto net_param = std::make_shared<NetParameterT*>();
+  auto net_param_t = std::make_shared<NetParameterT>();
+  auto net_param = net_param_t.get();
   const char* filename_r = "core/test_data/io_read_test.json";
-  ReadNetParamsFromTextFile(filename_r, net_param.get());
+  ReadNetParamsFromTextFile(filename_r, &net_param);
   const char* filename_w = "core/test_data/io_write_test.json";
-  WriteNetParamsToTextFile(*net_param.get(), filename_w);
+  WriteNetParamsToTextFile(net_param, filename_w);
 }
 
 TEST_F(IOTest, ReadWriteNetParamsFromBinaryFile) {
-  auto net_param = std::make_shared<NetParameterT*>();
+  auto net_param_t = std::make_shared<NetParameterT>();
+  auto net_param = net_param_t.get();
   const char* filename_r = "core/test_data/io_read_test.bin";
-  ReadNetParamsFromBinaryFile(filename_r, net_param.get());
+  ReadNetParamsFromBinaryFile(filename_r, &net_param);
   const char* filename_w = "core/test_data/io_write_test.bin";
-  WriteNetParamsToBinaryFile(*net_param.get(), filename_w);
+  WriteNetParamsToBinaryFile(net_param, filename_w);
 }
 
 }  // namespace mynet
