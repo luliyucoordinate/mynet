98ecdc995d24c01114f3985b5d9482b471d4011b
luliyu <coordinate@live.com>
2021-12-19 15:02:14 +0800

    fix error log

--- core/framework/tensor.cc
+++ core/framework/tensor.cc
@@ -271,7 +271,7 @@ Dtype Tensor<Dtype>::sumsq_diff() const {
     case SyncedMemory::UNINITIALIZED:
       return 0;
     default:
-      LOG(FATAL) << "Unknown SyncedMemory head state: " << data_->head();
+      LOG(FATAL) << "Unknown SyncedMemory head state: " << diff_->head();
   }
   return sumsq;
 }
