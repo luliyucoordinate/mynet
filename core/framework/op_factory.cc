f5ee4992bd51830e7b1b7eaadaa66ce17cafd95d
luliyu <coordinate@live.com>
2022-02-02 20:31:42 +0800

    register softmax

--- core/framework/op_factory.cc
+++ core/framework/op_factory.cc
@@ -6,6 +6,7 @@
 #include <memory>
 
 #include "core/kernels/conv_ops.hpp"
+#include "core/kernels/softmax_op.hpp"
 #include "op.hpp"
 
 namespace mynet {
@@ -16,7 +17,7 @@ std::shared_ptr<Op<Dtype>> GetConvOp(OpParameterT* param) {
   DCHECK(param);
   auto& conv_param = param->conv_param;
   DCHECK(conv_param);
-  auto engine = conv_param->engine;
+  auto& engine = conv_param->engine;
   if (engine == Engine_DEFAULT) {
     engine = Engine_MYNET;
   }
@@ -29,6 +30,26 @@ std::shared_ptr<Op<Dtype>> GetConvOp(OpParameterT* param) {
 }
 
 REGISTER_OP_CREATOR(Conv, GetConvOp);
+
+// Get softmax layer according to engine.
+template <typename Dtype>
+std::shared_ptr<Op<Dtype>> GetSoftmaxOp(OpParameterT* param) {
+  DCHECK(param);
+  auto& softmax_param = param->softmax_param;
+  DCHECK(softmax_param);
+  auto& engine = softmax_param->engine;
+  if (engine == Engine_DEFAULT) {
+    engine = Engine_MYNET;
+  }
+  if (engine == Engine_MYNET) {
+    return std::make_shared<SoftmaxOp<Dtype>>(param);
+  } else {
+    LOG(FATAL) << "Op " << param->name << " has unknown engine.";
+    throw;  // Avoids missing return warning
+  }
+}
+
+REGISTER_OP_CREATOR(Softmax, GetSoftmaxOp);
 
 // Op that use their constructor as their default creator should be
 // registered in their corresponding cpp files. Do not register them here.
