06d13cd5cc367f294548dff22c4d4b489739f5f9
luliyu <coordinate@live.com>
2022-02-03 16:08:25 +0800

    add split and trim

--- core/lib/format.hpp
+++ core/lib/format.hpp
@@ -7,13 +7,44 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <vector>
 
 namespace mynet {
 
-inline std::string format_int(int n, int zeros = 0) {
+inline std::string format_int(uint64_t n, uint32_t zeros = 0) {
   std::ostringstream s;
   s << std::setw(zeros) << std::setfill('0') << n;
   return s.str();
+}
+
+std::vector<std::string> split(const std::string &s, char delimiter) {
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+// trim from start (in place)
+void ltrim(std::string *s) {
+  s->erase(s->begin(), std::find_if(s->begin(), s->end(),
+                                    [](char ch) { return !std::isspace(ch); }));
+}
+
+// trim from end (in place)
+void rtrim(std::string *s) {
+  s->erase(std::find_if(s->rbegin(), s->rend(),
+                        [](char ch) { return !std::isspace(ch); })
+               .base(),
+           s->end());
+}
+
+// trim from both ends (in place)
+void trim(std::string *s) {
+  ltrim(s);
+  rtrim(s);
 }
 
 }  // namespace mynet
